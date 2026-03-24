aca0779554b0fdd0c292ae2d451ad276a6377754
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-03-24 10:29:41 +0100

    private fields

--- ClassesAndInterfaces/basics.ts
+++ ClassesAndInterfaces/basics.ts
@@ -9,9 +9,16 @@ class User {
 }
 
 class User2 {
-    constructor(public name: string, public age: number) { }
+    #privateJsField: string; // JavaScript private field
+    constructor(public name: string, private age: number) { this.#privateJsField = "This is a private field"; }
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+        console.log(this.#privateJsField);
+    }
 }
 
 let alice = new User2("Alice", 30);
 console.log(alice.name); // "Alice"
-console.log(alice.age);  // 30
+//console.log(alice.age);  // Error: Property 'age' is private and only accessible within class 'User2'.
+//alice.#privateJsField // Property '#privateJsField' is not accessible outside class 'User2' because it has a private identifier.ts(18013)
+alice.greet(); // "Hello, my name is Alice and I am 30 years old."
