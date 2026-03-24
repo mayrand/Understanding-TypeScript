848c5e46c73439efee51a4bca288d7f37dad3d0b
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-03-24 10:41:14 +0100

    Readonly fields

--- ClassesAndInterfaces/basics.ts
+++ ClassesAndInterfaces/basics.ts
@@ -1,6 +1,6 @@
 class User {
     name: string;
-    age: number;
+    readonly age: number;
 
     constructor(name: string, age: number) {
         this.name = name;
@@ -22,3 +22,7 @@ console.log(alice.name); // "Alice"
 //console.log(alice.age);  // Error: Property 'age' is private and only accessible within class 'User2'.
 //alice.#privateJsField // Property '#privateJsField' is not accessible outside class 'User2' because it has a private identifier.ts(18013)
 alice.greet(); // "Hello, my name is Alice and I am 30 years old."
+
+let bob = new User("Bob", 25);
+// bob.age = 26; // Error: Cannot assign to 'age' because it is a read-only property.
+console.log(bob.name + ' ' + bob.age); // "Bob 25"
