2216e577a61fd85c810a1e012034d670238b615f
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-03-26 09:41:32 +0100

    abstract class

--- ClassesAndInterfaces/advanced.ts
+++ ClassesAndInterfaces/advanced.ts
@@ -1,4 +1,4 @@
-class User {
+abstract class User {
     protected age: number = 0; // Protected property, accessible in subclasses if private it will not be accessible in subclasses.
     constructor(private name: string, private lastName: string) { }
     get fullName() {
@@ -22,12 +22,8 @@ class User {
     }
 }
 
-const user = new User("John", "Doe");
-console.log(user.fullName); // "John Doe"
+// const user = new User("John", "Doe"); // Error: Cannot create an instance of an abstract class.
 // user.fullName = "Jane Doe"; // Error: Cannot assign to 'fullName' because it is a read-only property.
-
-user.Age = 30;
-user.greet(); // "Hello, my name is John Doe. I am 30 years old."
 User.someStaticMethod(); // "This is a static method This is a static property"
 
 
