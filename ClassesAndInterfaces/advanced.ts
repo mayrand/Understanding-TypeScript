b172bb001c029c4ac5c22b50acf32db11180b04c
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-03-26 09:37:05 +0100

    Protected

--- ClassesAndInterfaces/advanced.ts
+++ ClassesAndInterfaces/advanced.ts
@@ -1,5 +1,5 @@
 class User {
-    private age: number = 0;
+    protected age: number = 0; // Protected property, accessible in subclasses if private it will not be accessible in subclasses.
     constructor(private name: string, private lastName: string) { }
     get fullName() {
         return this.name + " " + this.lastName;
@@ -37,11 +37,11 @@ class Admin extends User {
         super.Age = age;
     }
     greet() {
-        console.log(`Hello, my name is ${this.fullName}. I am an admin.`);
+        console.log(`Hello, my name is ${this.fullName}. I am an admin. I am ${this.age} years old.`);
     }
     static SomeIdentifier = "This is a static property for Admin";
 }
 
 const admin = new Admin("Alice", "Smith", 35);
-admin.greet(); // "Hello, my name is Alice Smith. I am an admin."
+admin.greet(); // "Hello, my name is Alice Smith. I am an admin. I am 35 years old."
 Admin.someStaticMethod(); // "This is a static method This is a static property"
