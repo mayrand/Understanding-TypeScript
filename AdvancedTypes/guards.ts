d010ce4ebf092760abc6d9392925e4652fc9fd2f
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-04-07 06:59:21 +0200

    Instanceof

--- AdvancedTypes/guards.ts
+++ AdvancedTypes/guards.ts
@@ -19,3 +19,32 @@ function loadData(source: Source) {
     }
     source.connectionUrl; // TypeScript knows this is a DBSource
 }
+
+class User {
+    constructor(public name: string) { }
+
+    join() {
+        // ...
+    }
+}
+
+class Admin {
+    constructor(permissions: string[]) { }
+
+    scan() {
+        // ...
+    }
+}
+
+const user = new User('Max');
+const admin = new Admin(['ban', 'restore']);
+
+type Entity = User | Admin;
+
+function init(entity: Entity) {
+    if (entity instanceof User) {
+        entity.join();
+        return;
+    }
+    entity.scan(); // TypeScript knows this is an Admin
+}
