5cc6f45b9c172a6782796e51af161ce5607b26a3
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-04-10 15:13:23 +0200

    as const

--- AdvancedTypes/advanced.ts
+++ AdvancedTypes/advanced.ts
@@ -16,3 +16,13 @@ store[3] = false;
 
 const sym = Symbol("unique");
 store[sym] = "Symbol Value";
+
+let roles = ["admin", "editor", "viewer"] as const;
+//roles.push("guest"); // Error: Property 'push' does not exist on type 'readonly ["admin", "editor", "viewer"]'.
+type Role = typeof roles[number];
+function assignRole(user: string, role: Role) {
+    console.log(`Assigned role ${role} to user ${user}`);
+}
+assignRole("Alice", "admin");
+assignRole("Bob", "editor");
+// assignRole("Charlie", "guest"); // Error: Argument of type '"guest"' is not assignable to parameter of type 'Role'.
