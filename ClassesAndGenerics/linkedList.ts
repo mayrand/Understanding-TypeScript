ef2e2c3bfd57af3bdaa07f79f17aecab621cc7f0
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-04-22 10:27:09 +0200

    Make classes generic and implement add method

--- ClassesAndGenerics/linkedList.ts
+++ ClassesAndGenerics/linkedList.ts
@@ -1,9 +1,27 @@
-class ListNode{}
+class ListNode<T>{
+    constructor(public value: T, public next?: ListNode<T>) { }
+}
 
-class LinkedList{
-    private root?: ListNode; // private is a typescript feature, in js Private fields are declared using #: #password = "secret";
+class LinkedList<T>{
+    private root?: ListNode<T>; // private is a typescript feature, in js Private fields are declared using #: #password = "secret";
     private length: number = 0;
+
+    add(value: T): void { 
+        const node = new ListNode(value);
+        if (!this.root) {
+            this.root = node;
+        } else {
+            let current = this.root;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.length++;
+    }
 }
 
 let list = new LinkedList();
 // list.root; // Error: Property 'root' is private and only accessible within class 'LinkedList'.
+list.add(1);
+list.add("Hello");
