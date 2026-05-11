898249ed892e56eeacdc683de08ee77e86a88e7f
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-05-11 12:49:03 +0200

    idexed access types

---
+++ Sekcja 10 Deriving Types From Types/indexed-access-types.ts
@@ -0,0 +1,26 @@
+// const appUser = {
+//     name: "John",
+//     age: 30,
+//     permissions: [
+//         {
+//             id: "1",
+//             title: "Admin",
+//             description: "Has full access"
+//         }
+//     ]
+// };
+
+// type AppUser = typeof appUser;
+
+type AppUser = {
+    name: string;
+    age: number;
+    permissions: {
+        id: string;
+        title: string;
+        description: string;
+    }[]
+}
+
+type Perms = AppUser["permissions"]; // Perms is now { id: string; title: string; description: string; }[]
+// idexed access types can also be used in any other type operations
