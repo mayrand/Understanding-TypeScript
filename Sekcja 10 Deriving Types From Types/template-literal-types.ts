70c61882ad8e607460034bad6d3450f3f976af8c
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-05-14 16:08:33 +0200

    Template literal type

--- Sekcja 10 Deriving Types From Types/template-literal-types.ts
+++ Sekcja 10 Deriving Types From Types/template-literal-types.ts
@@ -3,3 +3,9 @@ const userName = "John Doe";
 const greeting = `Hello, ${userName}! Welcome to TypeScript.`;
 
 console.log(greeting); // Output: Hello, John Doe! Welcome to TypeScript.
+
+type ReadPermissions = "read" | "no-read";
+type WritePermissions = "write" | "no-write";
+
+//type FilePermissions = 'no-read-write' | 'read-no-write' | 'read-write' | 'no-read-no-write';
+type FilePermissions = `${ReadPermissions}-${WritePermissions}`;
