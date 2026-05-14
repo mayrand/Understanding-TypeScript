43ec994bc9bef37efd567376ef83ea423571a212
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-05-14 15:56:30 +0200

    read-only property

--- Sekcja 10 Deriving Types From Types/mapped-types.ts
+++ Sekcja 10 Deriving Types From Types/mapped-types.ts
@@ -1,6 +1,6 @@
 type Operations = {
-    add?: (a: number, b: number) => number;
-    subtract?: (a: number, b: number) => number;
+    add: (a: number, b: number) => number;
+    subtract: (a: number, b: number) => number;
 };
 
 let mathOperations: Operations = {
@@ -21,7 +21,7 @@ let mathOperations: Operations = {
 // The above code is fine, but it can be easily broken if we add more operations to the Operations type. We would have to manually update the Results1 type and the mathResults1 object. This is where mapped types come in handy.
 
 type Results<T> = {
-    [K in keyof T]-?: number; // Operations are optional in the Operations type, but we want them to be required in the Results type. The -? removes the optional modifier from the properties.
+    readonly [K in keyof T]?: number; // Operations are optional in the Operations type, but we want them to be required in the Results type. The -? removes the optional modifier from the properties.
 };
 
 let mathResults: Results<Operations> = {
@@ -29,3 +29,4 @@ let mathResults: Results<Operations> = {
     subtract: mathOperations.subtract!(5, 2) // Exclamation mark is used to assert that the properties are not undefined, since we know that they will be defined in this context.
 };
 
+//mathResults.add = 10; // Error: Cannot assign to 'add' because it is a read-only property. Readonly can be used on normal properties too.
