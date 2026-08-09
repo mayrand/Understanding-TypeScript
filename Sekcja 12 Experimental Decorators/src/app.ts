c8877486b8a424dc0e8185115f787c2808e295c4
andrzej <andrzej.oldak@gmail.com>
2026-08-09 13:54:57 +0200

    159. Fixing a Validator Bug

--- Sekcja 12 Experimental Decorators/src/app.ts
+++ Sekcja 12 Experimental Decorators/src/app.ts
@@ -122,15 +122,15 @@ const registeredValidators: ValidatorConfig = {}
 function Required(target: any, propName: string) {
     registeredValidators[target.constructor.name] = {
         ...registeredValidators[target.constructor.name],
-        [propName]: ['required']
-    }
+        [propName]: [...(registeredValidators[target.constructor.name]?.[propName] ?? []), 'required']
+    };
 }
 
 function PositiveNumber(target: any, propName: string) {
     registeredValidators[target.constructor.name] = {
         ...registeredValidators[target.constructor.name],
-        [propName]: ['positive']
-    }
+        [propName]: [...(registeredValidators[target.constructor.name]?.[propName] ?? []), 'positive']
+    };
 }
 
 function validate(obj: any) {
