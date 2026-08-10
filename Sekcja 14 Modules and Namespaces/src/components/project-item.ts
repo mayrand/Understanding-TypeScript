2ccbd845c518dfd44fd6e5642edc76c2deb34b60
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-08-10 16:47:20 +0200

    191. Understanding "type" Imports Można dać type żeby toole wiedziały że importujesz typy.

--- Sekcja 14 Modules and Namespaces/src/components/project-item.ts
+++ Sekcja 14 Modules and Namespaces/src/components/project-item.ts
@@ -1,7 +1,7 @@
 import { Project } from '../models/project.js';
 import Component from './base-component.js';
 import { autobind } from '../decorators/autobind.js';
-import { Draggable } from '../models/drag-drop.js';
+import { type Draggable } from '../models/drag-drop.js';
 
 // ProjectItem Class
 export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
--- Sekcja 14 Modules and Namespaces/src/components/project-list.ts
+++ Sekcja 14 Modules and Namespaces/src/components/project-list.ts
@@ -3,7 +3,7 @@ import Component from './base-component.js';
 import { autobind } from '../decorators/autobind.js';
 import { projectState } from '../state/project-state.js';
 import { ProjectItem } from './project-item.js';
-import { DragTarget } from '../models/drag-drop.js';
+import type { DragTarget } from '../models/drag-drop.js';
 
 export class ProjectList extends Component<HTMLDivElement, HTMLElement>
   implements DragTarget {
