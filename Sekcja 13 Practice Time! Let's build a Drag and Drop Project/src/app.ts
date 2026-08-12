8b55a65df57ac2da2010544771296ad04ace9f55
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-08-12 16:14:05 +0200

    166. Creating & Using an "Autobind" Decorator - autobind decorator

--- Sekcja 13 Practice Time! Let's build a Drag and Drop Project/src/app.ts
+++ Sekcja 13 Practice Time! Let's build a Drag and Drop Project/src/app.ts
@@ -1,3 +1,15 @@
+function autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -22,6 +34,7 @@ class ProjectInput {
         this.attach();
     }
 
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
@@ -29,7 +42,7 @@ class ProjectInput {
 
     private configure() {
         // this.element.addEventListener('submit', this.submitHandler); -- Uncaught TypeError: Cannot read properties of undefined (reading 'value') at HTMLFormElement.submitHandler (app.ts:27:44)
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
