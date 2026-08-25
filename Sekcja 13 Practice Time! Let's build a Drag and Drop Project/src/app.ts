f7d5083a15ada0671f410ba6ca7cacbfe053a5df
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-08-25 11:08:41 +0200

    175. Using a Getter

--- Sekcja 13 Practice Time! Let's build a Drag and Drop Project/src/app.ts
+++ Sekcja 13 Practice Time! Let's build a Drag and Drop Project/src/app.ts
@@ -227,6 +227,14 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     private project: Project;
+
+    get persons() {
+        if (this.project.people === 1)
+            return '1 person';
+        else
+            return `${this.project.people} persons`
+    }
+
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, false, project.id);
         this.project = project;
@@ -236,7 +244,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     configure(): void { }
     renderContent(): void {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
         this.element.querySelector('p')!.textContent = this.project.description;
     }
 }
