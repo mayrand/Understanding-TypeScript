411981db0be89e0b9f6a3c2a5393307299a740a4
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-08-25 11:04:50 +0200

    174. Rendering Project Items with a Class

--- Sekcja 13 Practice Time! Let's build a Drag and Drop Project/src/app.ts
+++ Sekcja 13 Practice Time! Let's build a Drag and Drop Project/src/app.ts
@@ -151,9 +151,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
         listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl?.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }
 }
@@ -223,6 +221,23 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
             projectState.addProject(title, desc, people);
             this.clearInputs();
         }
+    }
+}
+
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.renderContent();
+    }
+
+    configure(): void { }
+    renderContent(): void {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
     }
 }
 
