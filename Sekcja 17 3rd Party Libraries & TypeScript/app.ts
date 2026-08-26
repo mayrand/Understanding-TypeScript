6a45bd1f72bc7865dcdedac3c40821130b933180
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-08-26 11:14:20 +0200

    222. Diving Deeper Into Zod

--- Sekcja 17 3rd Party Libraries & TypeScript/app.ts
+++ Sekcja 17 3rd Party Libraries & TypeScript/app.ts
@@ -1,6 +1,11 @@
 import fs from 'node:fs';
 import { z } from 'zod';
 
-const dataSchema = z.string();
-const content = fs.readFileSync('data.json');
+const dataSchema = z.object({
+    title: z.string(),
+    id: z.number(),
+    values: z.array(z.string())
+});
+const content = JSON.parse(fs.readFileSync('data.json').toString());
 const parsedData = dataSchema.parse(content);
+console.log(parsedData.title);
