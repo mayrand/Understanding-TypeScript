04e343124d6548a30a02e10b80c419619ba084e2
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-09-02 14:34:59 +0200

    242. Working with Refs

--- Sekcja 19 React.js and TypeScript/src/components/NewGoal.tsx
+++ Sekcja 19 React.js and TypeScript/src/components/NewGoal.tsx
@@ -1,20 +1,24 @@
-import type { FormEvent } from "react";
+import { useRef, type FormEvent } from "react";
 
 export default function NewGoal() {
+    const goalRef = useRef<HTMLInputElement>(null);
+    const summaryRef = useRef<HTMLInputElement>(null);
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
-
+        const enteredGoal = goalRef.current!.value;
+        const enteredSummary = summaryRef.current!.value;
+        
     }
 
     return <form onSubmit={handleSubmit}>
         <p>
             <label htmlFor="goal">Your goal</label>
-            <input id="goal" type="text" />
+            <input id="goal" type="text" ref={goalRef} />
         </p>
         <p>
             <label htmlFor="summary">Your summary</label>
-            <input id="summary" type="text" />
+            <input id="summary" type="text" ref={summaryRef} />
         </p>
         <p>
             <button>Add goal</button>
