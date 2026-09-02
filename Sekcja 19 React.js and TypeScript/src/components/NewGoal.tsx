b84c45faf6463e2213ad90cbff331c1fb5479f0f
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-09-02 14:29:35 +0200

    241. Handling Form Submission

---
+++ Sekcja 19 React.js and TypeScript/src/components/NewGoal.tsx
@@ -0,0 +1,23 @@
+import type { FormEvent } from "react";
+
+export default function NewGoal() {
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+
+    }
+
+    return <form onSubmit={handleSubmit}>
+        <p>
+            <label htmlFor="goal">Your goal</label>
+            <input id="goal" type="text" />
+        </p>
+        <p>
+            <label htmlFor="summary">Your summary</label>
+            <input id="summary" type="text" />
+        </p>
+        <p>
+            <button>Add goal</button>
+        </p>
+    </form>
+}
