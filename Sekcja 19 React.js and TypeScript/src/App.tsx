275f2ecf2fb135eae6bb2a6d40eefb395140bd7e
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-09-02 14:19:07 +0200

    239. Managing State

--- Sekcja 19 React.js and TypeScript/src/App.tsx
+++ Sekcja 19 React.js and TypeScript/src/App.tsx
@@ -1,16 +1,20 @@
 import Header from "./components/Header"
 import goalsImg from "./assets/goals.jpg"
 import CourseGoals from "./components/CourseGoals"
+import { useState } from "react"
 
 function App() {
+  const [goals, setGoals] = useState([
+    { id: 1, title: 'Learn TS', description: 'Learn Ts from the ground up' },
+    { id: 2, title: 'Practice TS', description: 'Practice Ts from the ground up' }
+  ]);
 
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'List of goals' }}>
         <h1>Your course goals</h1>
       </Header>
-      <CourseGoals goals={[{ id: 1, title: 'Learn TS', description: 'Learn Ts from the ground up' }]} />
-      <CourseGoals goals={[{ id: 2, title: 'Practice TS', description: 'Practice Ts from the ground up' }]} />
+      <CourseGoals goals={goals} />
     </main>
   )
 }
