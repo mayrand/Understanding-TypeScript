ac460341666e8ca508cbefe7b000a0a9f877c6d8
Andrzej Ołdak <andrzej.oldak@diag.pl>
2026-09-02 13:45:45 +0200

    235. Using Components & Props

--- Sekcja 19 React.js and TypeScript/src/App.tsx
+++ Sekcja 19 React.js and TypeScript/src/App.tsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header"
+import goalsImg from "./assets/goals.jpg"
+
 function App() {
 
   return (
-    <>
+    <main>
+      <Header image={{src: goalsImg, alt: 'List of goals'}} />
       <h1>Hello world!</h1>
-    </>
+    </main>
   )
 }
 
